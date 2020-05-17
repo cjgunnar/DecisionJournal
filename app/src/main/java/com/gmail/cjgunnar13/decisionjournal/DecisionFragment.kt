7c88b3e08bda269fda351d188b113c07ec715ae5
cjgunnar <34259593+cjgunnar@users.noreply.github.com>
2020-05-16 19:02:10 -0500

    add empty decision view/edit fragment

---
+++ app/src/main/java/com/gmail/cjgunnar13/decisionjournal/DecisionFragment.kt
@@ -0,0 +1,26 @@
+package com.gmail.cjgunnar13.decisionjournal
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import java.util.*
+
+class DecisionFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_decision, container, false)
+        return view
+    }
+
+    companion object {
+        fun newInstance(decisionID : UUID) : DecisionFragment {
+            return DecisionFragment()
+        }
+    }
+}
---
+++ app/src/main/java/com/gmail/cjgunnar13/decisionjournal/DecisionViewModel.kt
@@ -0,0 +1,6 @@
+package com.gmail.cjgunnar13.decisionjournal
+
+import androidx.lifecycle.ViewModel
+
+class DecisionViewModel : ViewModel() {
+}
