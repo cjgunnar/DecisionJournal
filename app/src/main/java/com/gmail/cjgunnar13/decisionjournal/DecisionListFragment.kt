a9418d2ccc3579d1626945e9143e47da01871a9b
cjgunnar <34259593+cjgunnar@users.noreply.github.com>
2020-05-17 01:36:31 -0500

    add recyclerview functionality

--- app/src/main/java/com/gmail/cjgunnar13/decisionjournal/DecisionListFragment.kt
+++ app/src/main/java/com/gmail/cjgunnar13/decisionjournal/DecisionListFragment.kt
@@ -5,6 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 /**
@@ -15,6 +18,12 @@ import androidx.recyclerview.widget.RecyclerView
 class DecisionListFragment : Fragment() {
     private lateinit var decisionRecyclerView: RecyclerView
 
+    private var adapter: DecisionAdapter = DecisionAdapter(emptyList())
+
+    private val model: DecisionListViewModel by lazy {
+        ViewModelProvider(this@DecisionListFragment).get(DecisionListViewModel::class.java)
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
@@ -22,9 +31,67 @@ class DecisionListFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.fragment_decision_list, container, false)
 
-        //create elements...
+        //initialize UI elements
+        decisionRecyclerView = view.findViewById(R.id.rv_fdl_decision_list)
+        decisionRecyclerView.apply {
+            layoutManager = LinearLayoutManager(context)
+            adapter = this@DecisionListFragment.adapter
+        }
 
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        //create data listeners
+
+        //observe decision list and update UI if changed
+        model.decisionsLiveData.observe(
+            viewLifecycleOwner,
+            Observer { decisions ->
+                decisions?.let {
+                    updateUI(it)
+                }
+            }
+        )
+    }
+
+    /**
+     * update adapter on recyclerview to show new list
+     */
+    private fun updateUI(decisions: List<Decision>) {
+        adapter = DecisionAdapter(decisions)
+        decisionRecyclerView.adapter = adapter
+    }
+
+    //RECYCLER
+    private inner class DecisionHolder(view: View) : RecyclerView.ViewHolder(view) {
+        //UI elements
+
+        //decision to display
+        private var decision: Decision? = null
+
+        fun bind(decision: Decision) {
+            this.decision = decision
+
+            //update UI elements
+        }
+    }
+
+    private inner class DecisionAdapter(var decisions: List<Decision>) :
+        RecyclerView.Adapter<DecisionHolder>() {
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DecisionHolder {
+            val view = layoutInflater.inflate(R.layout.list_item_decision, parent)
+            return DecisionHolder(view)
+        }
+
+        override fun getItemCount() = decisions.size
+
+        override fun onBindViewHolder(holder: DecisionHolder, position: Int) {
+            holder.bind(decisions[position])
+        }
+
     }
 
     companion object {
--- app/src/main/java/com/gmail/cjgunnar13/decisionjournal/DecisionListViewModel.kt
+++ app/src/main/java/com/gmail/cjgunnar13/decisionjournal/DecisionListViewModel.kt
@@ -3,5 +3,9 @@ package com.gmail.cjgunnar13.decisionjournal
 import androidx.lifecycle.ViewModel
 
 class DecisionListViewModel : ViewModel()  {
-    //repository, live data
+    //get singleton repository
+    private val decisionRepository = DecisionRepository.get()
+
+    //access methods
+    val decisionsLiveData = decisionRepository.getDecisions()
 }
